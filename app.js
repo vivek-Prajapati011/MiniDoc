5af325c785e051586c2b1c5bf30b8271d38eb062
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-08 18:57:46 +0530

    setting cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from "express"
+import cors from "cors"
 const app = express()
- const port = 3000
+const port = 3000
 
+app.use(cors())
  app.listen(port, () => {
     console.log("server is runing on this port", port)
  }) 
