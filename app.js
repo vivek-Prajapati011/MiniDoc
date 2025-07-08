565d88bc9375d1f34cc2dfb276e675c7e6ae13e3
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-08 19:17:47 +0530

    settting middleware for all routes

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import express from "express"
 import cors from "cors"
+import {readdir} from "fs/promises"
 const app = express()
 const port = 3000
 
 app.use(cors())
+app.get("/",  async (req,res) =>{ 
+    const fileList = await readdir("./storage") 
+    console.log(fileList)
+    res.json(fileList)
+})
  app.listen(port, () => {
     console.log("server is runing on this port", port)
  }) 
