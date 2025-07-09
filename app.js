bb753c4a76c14c642ffc4597e5ffa9505168a3e9
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-09 22:26:51 +0530

    setting storage folder path

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 import express from "express"
 import cors from "cors"
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
 import {readdir} from "fs/promises"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express()
 const port = 3000
 
 app.use(cors())
 app.get("/",  async (req,res) =>{ 
-    const fileList = await readdir("./storage") 
-    console.log(fileList)
-    res.json(fileList)
+   const folderPath = path.join(__dirname, "Storage");
+
+    const fileList = await readdir(folderPath);
+    res.json(fileList);
 })
  app.listen(port, () => {
     console.log("server is runing on this port", port)
