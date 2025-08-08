df86864f8a22f4148a305d89b7bb4d62f2588dda
vivek prajapati <vivek011prajapati@gmail.com>
2025-08-08 07:36:13 +0530

    import rename, rm

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-import {readdir} from "fs/promises"
+import {readdir,rename, rm } from "fs/promises"
 import { createWriteStream } from "fs"
 
 const app = express()
