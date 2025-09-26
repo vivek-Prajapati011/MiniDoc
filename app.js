1b12b5271d353484a09c529e251012c56a396a4a
vivek prajapati <vivek011prajapati@gmail.com>
2025-09-26 23:27:10 +0530

    feat: mount the user rout

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import directoryRoutes from "./routes/directoryRoute.js";
 import fileRoutes from "./routes/fileRoute.js";
+import userRoutes from "./routes/userRoute.js";
 
 const app = express();
 const PORT = 3000;
@@ -12,6 +13,7 @@ app.use(cors());
 // Mount Routers
 app.use("/directory", directoryRoutes);
 app.use("/files", fileRoutes);
+app.use("/users", userRoutes)
 
 app.listen(PORT, () => {
   console.log(`✅ Server started on http://localhost:${PORT}`);
