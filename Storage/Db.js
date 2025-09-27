78747589a57fdeae9a7efd68c1ba15c914f5b6d8
vivek prajapati <vivek011prajapati@gmail.com>
2025-09-27 22:54:54 +0530

    feat: make async function that connnet to  db

--- Storage/Db.js
+++ Storage/Db.js
@@ -1,3 +1,8 @@
 import { MongoClient } from "mongodb";
-const client = new MongoClient("mongodb://127.0.0.1:27017/storageApp")
-await client.connect()
+const client = new MongoClient("mongodb://127.0.0.1:27017/storageApp");
+
+ export async function connectDb() {
+  await client.connect();
+  const db = client.db();
+  return db
+}
