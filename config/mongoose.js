4a85d069c71d55f3d7e7ac49e161909ee396e882
vivek prajapati <vivek011prajapati@gmail.com>
2025-11-13 05:41:44 +0530

    establish mongoose connection

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,3 +1,7 @@
 import mongoose from "mongoose";
 
-await mongoose.connect("mongodb://127.0.0.1:27017/storageApp")
+await mongoose.connect("mongodb://127.0.0.1:27017/storageApp");
+
+
+mongoose.connection.on("connected", () => console.log("✅ MongoDB Connected"));
+mongoose.connection.on("error", (err) => console.error("❌ MongoDB Error:", err));
