c186e1bc913506eec40eb50827ab72fac7166d7f
vivek prajapati <vivek011prajapati@gmail.com>
2025-11-13 05:47:05 +0530

    make schema of file

--- models/fileModel.js
+++ models/fileModel.js
@@ -1,29 +1,13 @@
 import { model, Schema } from "mongoose";
 
-const fileSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    extension: {
-      type: String,
-      required: true,
-    },
-    userId: {
-      type: Schema.Types.ObjectId,
-      required: true,
-    },
-    parentDirId: {
-      type: Schema.Types.ObjectId,
-      default: null,
-      ref: "Directory",
-    },
-  },
-  {
-    strict: "throw", 
-  }
-);
+const fileSchema = new Schema({
+  name: { type: String, required: true },
+  extension: { type: String },
+  storedName: { type: String },
+  size: { type: Number },
+  userId: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+  parentDirId: { type: Schema.Types.ObjectId, ref: "Directory", default: null },
+});
 
-const File = model("File", fileSchema)
-export default File
+const File = model("File", fileSchema);
+export default File;
